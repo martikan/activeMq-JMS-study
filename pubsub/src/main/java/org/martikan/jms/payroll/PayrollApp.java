c834b9b692795451ec78d5d0cd5b0b7b2eceeb00
Richard Martikan <richard.martikan@avinty.com>
2023-04-18 18:34:18 +0200

    pub-sub added

---
+++ pubsub/src/main/java/org/martikan/jms/payroll/PayrollApp.java
@@ -0,0 +1,32 @@
+package org.martikan.jms.security;
+
+import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
+import org.martikan.jms.hr.model.Employee;
+
+import javax.jms.JMSContext;
+import javax.jms.JMSException;
+import javax.jms.Topic;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public class SecurityApp {
+
+    public static void main(String[] args) throws NamingException, JMSException {
+
+        final var ctx = new InitialContext();
+
+        final var empTopic = (Topic) ctx.lookup("topic/empTopic");
+
+        try (ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory();
+             JMSContext jmsContext = cf.createContext()) {
+            final var consumer = jmsContext.createConsumer(empTopic);
+
+            final var message = consumer.receive();
+
+            final var employee = message.getBody(Employee.class);
+
+            System.out.println(employee.getEmail());
+        }
+    }
+
+}
